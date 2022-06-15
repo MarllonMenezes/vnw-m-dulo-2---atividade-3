66a4d616d17f60fb4b549670da0d25d141ae5ac2
marllondallas <marllon_dallas@hotmail.com>
2022-06-15 16:24:44 -0300

    terceira atividade vnw módulo 2

---
+++ main.js
@@ -0,0 +1,22 @@
+
+1 - Criar um loop que conte de 1 até 10 usando FOR
+for(let numero = 0; numero <= 10; numero++ ){
+  console.log(numero)
+}
+
+2 - Criar um loop que conte de 10 até 1 usando WHILE
+let n = 0;
+while (n <= 10){
+ console.log(n)
+ n++;
+}
+
+3 - Criar um loop que conte todos os números ímpares de 1 até 100
+ for (let numero = 1; numero <= 100; numero += 2){
+    console.log(numero)
+ }
+
+4- Criar um loop que conte todos os números pares de 0 a 100
+for (let numero = 0; numero <= 100; numero += 2){
+    console.log(numero)
+ }
